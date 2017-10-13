e07334a6e932f593fc9fc9f0911a746d44efdd61
paulfarcas <farcaspaul@yahoo.com>
2017-10-13 11:33:43 -0400

    2

--- src/library/Book.java
+++ src/library/Book.java
@@ -9,7 +9,6 @@ public class Book {
     
     Book() {
         System.out.println("You have to set a name, an author, a price and a quantity for this book.");
-        System.out.println("Push");
     }
     
     Book(String title, Author author, double price, int quantity) {
